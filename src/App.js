eea9ed7f5e681802e6cbb6e45d6308f948671c27
Alejandro Servin <alejandro.servin@upa.edu.py>
2019-10-07 17:22:02 -0300

    actualización

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ const styles = theme => ({
   },
 });
 
-
 function Productos({match}){
   return(
     <>
--- src/componentes/productos/ListaProductos.js
+++ src/componentes/productos/ListaProductos.js
@@ -5,7 +5,6 @@ import {BrowserRouter as Router, Link} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
-// import Paper from '@material-ui/core/Paper';
 
 import Producto from './Producto.js'
 
